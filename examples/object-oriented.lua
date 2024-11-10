957a56e5b2dc57a30a6b16511bd336deefbe737b
Thijs Schreijer <thijs@thijsschreijer.nl>
2024-11-10 21:22:52 +0100

    chore(docs): update and render docs

---
+++ examples/object-oriented.lua
@@ -0,0 +1,19 @@
+-- using `ctx` to pass `self` to the callback function.
+
+local Mediator = require 'mediator'
+local m = Mediator()
+
+
+local myObject = {
+  noise_count = 0
+}
+
+function myObject:makeNoiseHandler(noise)
+  self.noise_count = self.noise_count + 1
+  print(self.noise_count, noise)
+end
+
+local c = m:getChannel({"animal", "cat"})
+c:addSubscriber(myObject.makeNoiseHandler, { ctx = myObject })
+m:publish("meow")
+
